d91570d5c8f1446047382588787c121b9be5ce10
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-09-01 20:10:20 -0700

    Update webgen.py

--- webgen.py
+++ webgen.py
@@ -1,4 +1,4 @@
-import webbrowser
+import webbrowser 
 from tkinter import *
 import tkinter as tk
 
@@ -6,14 +6,14 @@ def load_gui(self):
 
     self.entry1=tk.Entry(self.master)
     self.entry1.grid(row=0,column=0)
-    self.txt_button=tk.Button(self.master,width=20,height=2,text='Open HTML file')
+    self.txt_button=tk.Button(self.master,width=20,height=2,text='Open HTML file', command=lambda:Submit(self)) #button widget uses command keyword to call submit function when clicked
     self.txt_button.grid(row=1,column=0)
         
 
 
     def Submit(self):
         txt=self.entry1.get()
-        f = open('test.html','w')
+        f = open('test.html','w') #variable f that uses method to open html file
 
         message = """
         <html>
@@ -24,9 +24,9 @@ def load_gui(self):
         
         f.write(message)
         f.close()
-        webbrowser.open_new_tab('test.html')
+        webbrowser.open_new_tab('test.html') #HTML opens in browser
 
-        self.txt_button=tk.Button(self.master,width=20,height=2,text='Open HTML file', command=lambda:Submit(self))
+        
         
 
         
